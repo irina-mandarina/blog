e610ed9399bf3f721626074f722598a585b290ed
Irina Ahamad <irina.ahamad@gmail.com>
2025-01-29 14:12:13 +0200

    Blog Post model, Interaction Model

--- project/project/urls.py
+++ project/project/urls.py
@@ -12,7 +12,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blogs/', include('blogs.urls'))
 """
 
 from django.contrib import admin
@@ -20,4 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("blogs/", include("blogs.urls"))
 ]
